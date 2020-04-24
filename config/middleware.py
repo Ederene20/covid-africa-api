6696d69e38a978d2ef0bfebaaf44687ac568bcf7
Kolawole <onaelmangabo@gmail.com>
2020-04-24 16:49:36 +0200

    Resolve routes issue

--- config/middleware.py
+++ config/middleware.py
@@ -36,7 +36,7 @@ ROUTE_MIDDLEWARE = {
 
 CORS = {
     'Access-Control-Allow-Origin': "*",
-    "Access-Control-Allow-Methods": "DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT",
+    "Access-Control-Allow-Methods": "GET, HEAD, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Accept, X-Requested-With",
     "Access-Control-Max-Age": "3600",
     "Access-Control-Allow-Credentials": "true"
